c04487d8b8ec7b940edda52b572acec3eccd3fe0
Ceazy-Gentleman <13672215525@163.com>
2021-04-01 16:37:30 +0800

    添加项目文件。

---
+++ gg.h
@@ -0,0 +1,11 @@
+#pragma once
+#include<iostream>
+#include<string>
+#include<vector>
+using namespace std;
+
+template<typename T>
+T SUM(T a, T b)
+{
+	return a + b;
+}
---
+++ hhh.cpp
@@ -0,0 +1,8 @@
+#include "gg.h"
+
+
+int main() {
+	cout << SUM(2, 3) << endl;
+	cout << "ÁõÖ¾Åô" << endl;
+	return 0;
+}
